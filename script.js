f2fce759a4b0253f83d7c56bb311f76344de03e0
luangly <lehachess.com@gmail.com>
2025-11-20 19:56:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ const sections = document.querySelectorAll('.section');
 const navBtns = document.querySelectorAll('.nav-btn');
 
 function showSection(index) {
+    // Барлық секцияларды жасыру
     sections.forEach(section => section.classList.remove('active'));
+    // Барлық кнопкаларды "active" класстан шығару
     navBtns.forEach(btn => btn.classList.remove('active'));
 
+    // Таңдалған секцияны көрсету
     sections[index].classList.add('active');
+    // Таңдалған кнопканы көрсету
     navBtns[index].classList.add('active');
 
+    // Бетті жоғарғы жағына жылжыту
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
